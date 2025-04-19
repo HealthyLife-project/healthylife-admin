f6e25bdfd7665e580ded4ecb4f3189a0c4a36def
ash <tkdgys1234@gmail.com>
2025-04-19 20:52:12 +0900

    도메인 변경

--- healthy-admin/src/pages/users/report/index.tsx
+++ healthy-admin/src/pages/users/report/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Reports from "@/features/report/report"; // AdBanner 컴포넌트를 import 합니다.
+import Reports from "@/features/report/report";
 
 const ReportPage = () => {
   return (
